8d19136e99e66c9fdda94d655ca11501b6f83c08
Academmia <academmia.com@gmail.com>
2020-11-09 09:30:28 +0200

    'init'

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Academmia</h1>
+      <h3>useReducer Hook</h3>
+      <hr />
+      <div style={{ border: 'solid 1px #ccc', minHeight: '400px' }}>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
